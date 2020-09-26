c0efa3dc18f90e74f820573a32ff0ba62d809cc4
Mark Monday <mark.monday0504@gmail.com>
2020-09-26 01:02:22 -0400

    fix feedback api endpoint

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,6 +1,6 @@
 import nc from 'next-connect';
 import { sendSupportEmail } from '../../server/services/support-email-service';
 
-export default nc().get(async function (req, res) {
+export default nc().post(async function (req, res) {
 	res.json(await sendSupportEmail(req.body));
 });
